962a2b795441e4068e0206cb1b42f20c6c1e4621
alexvlamakis <alexvlamakis@gmail.com>
2021-12-26 13:37:21 -0700

    ## ID's for display of data from user input

--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -11,9 +11,34 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+
+// title for readMe to pull data from user input inclusive of licensing display.
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ## Licensing:
+  [![license](https://img.shields.io/badge/license-${data.licensing}-blue)](https://shields.io)
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Additional Info](#additional-info)
+  ## Description:
+  ${data.description}
+  ## Installation:
+  ${data.installation}
+  ## Usage:
+  ${data.usage}
+  ## License:
+  ${data.licensing}
+  ## Contribution:
+  ${data.contribution}
+  ## Testing:
+  ${data.testing}
+  ## Additional Info:
+  - Github: [${data.github}](https://github.com/${data.github})
+  - Email: ${data.email}
 `;
 }
 
